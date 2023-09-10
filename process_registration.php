8790f1203bbd026a0a731d5a6184ee8b3097e79e
RAMELZ <144543801+RAMELZ@users.noreply.github.com>
2023-09-10 14:25:39 +0800

    Add files via upload
    MAIN

--- process_registration.php
+++ process_registration.php
@@ -7,39 +7,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Basic validation (you can add more complex validation as needed)
     if ($password !== $confirm_password) {
-        echo "Passwords do not match.";
+        echo "if it's correct any confirm password registration successful.";
     } else {
-        // Hash the password
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        // Store the user data or perform other actions (e.g., database insertion)
+        // You should hash the password before storing it in a real application
 
-        // Establish a database connection (replace with your database details)
-        $servername = "your_servername";
-        $db_username = "your_username";
-        $db_password = "your_password";
-        $db_name = "your_db_name";
-
-        $conn = new mysqli($servername, $db_username, $db_password, $db_name);
-
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-
-        // Prepare and execute an SQL query to insert the user data
-        $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("sss", $username, $email, $hashed_password);
-
-        if ($stmt->execute()) {
-            echo "Registration successful!<br>";
-            echo "Username: " . $username . "<br>";
-            echo "Email: " . $email . "<br>";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-
-        // Close the database connection
-        $stmt->close();
-        $conn->close();
+        // For demonstration purposes, we'll just display the user's data
+        echo "Registration successful!<br>";
+        echo "Username: " . $username . "<br>";
+        echo "Email: " . $email . "<br>";
     }
 }
 ?>
